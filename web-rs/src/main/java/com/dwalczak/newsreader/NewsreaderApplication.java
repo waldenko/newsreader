aa7af3ce806743780e2907dbf88058ff1adfd3c8
Dawid Walczak <dawid.walczak@asseco.pl>
2018-03-20 23:43:28 +0100

    Dodaj logowanie komunikatów z/do usług REST

--- web-rs/src/main/java/com/dwalczak/newsreader/NewsreaderApplication.java
+++ web-rs/src/main/java/com/dwalczak/newsreader/NewsreaderApplication.java
@@ -1,9 +1,14 @@
 package com.dwalczak.newsreader;
 
+import com.dwalczak.newsreader.filter.RestLoggingFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -13,4 +18,14 @@ public class NewsreaderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NewsreaderApplication.class, args);
 	}
+
+	@Bean
+	public FilterRegistrationBean restLoggingFilter() {
+		FilterRegistrationBean<RestLoggingFilter> registration = new FilterRegistrationBean<>();
+		registration.setFilter(new RestLoggingFilter());
+		registration.addUrlPatterns("/news/*");
+		registration.setOrder(HIGHEST_PRECEDENCE);
+		return registration;
+	}
+
 }
---
+++ web-rs/src/main/java/com/dwalczak/newsreader/filter/RestLoggingFilter.java
@@ -0,0 +1,58 @@
+package com.dwalczak.newsreader.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+public class RestLoggingFilter extends OncePerRequestFilter {
+    private static final String CHARSET = "UTF-8";
+    private static final Logger apiLog = org.slf4j.LoggerFactory.getLogger("api");
+
+
+    @Override protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        BufferedRequestWrapper wrappedRequest = new BufferedRequestWrapper(request);
+        ContentCachingResponseWrapper wrappedResponse = new ContentCachingResponseWrapper(response);
+        traceReq(wrappedRequest);
+        try {
+            filterChain.doFilter(wrappedRequest, wrappedResponse);
+        } finally {
+            traceRes(wrappedResponse);
+            wrappedResponse.copyBodyToResponse();
+        }
+    }
+
+    private void traceReq(BufferedRequestWrapper request) {
+        apiLog.info(formatLogParams(Arrays.asList(
+                request.getMethod(),
+                request.getRequestURI()))
+                + " " + request.getRequestBody())
+        ;
+    }
+
+    private void traceRes(ContentCachingResponseWrapper response) throws IOException {
+        String body = "";
+        try {
+            body = IOUtils.toString(response.getContentAsByteArray(), CHARSET);
+        } catch (Exception e) {
+            log.warn("Can't get response body", e);
+        }
+        apiLog.info(Arrays.asList(response.getStatusCode()) + " " + body);
+    }
+
+    private String formatLogParams(List<Object> params) {
+        StringBuilder buf = new StringBuilder();
+        params.forEach(s -> {if (s != null) buf.append('[').append(s).append(']');});
+        return buf.toString();
+    }
+}
