fc59028d7751f71424cfa6f94ff79c4dddd648d5
Dawid Walczak <dawid.walczak@asseco.pl>
2018-03-22 23:02:40 +0100

    Dodaj testy jednostkowe

---
+++ web-rs/src/test/groovy/com/dwalczak/newsreader/rs/mapper/ArticleFilterMapperSpec.groovy
@@ -0,0 +1,31 @@
+package com.dwalczak.newsreader.rs.mapper
+
+import com.dwalczak.newsreader.model.ArticleFilter
+import com.dwalczak.newsreader.model.PaginationFilter
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class ArticleFilterMapperSpec extends Specification {
+
+
+    @Unroll
+    def "check filter mapping: #srcDto"() {
+        given:
+        dstDto.country = srcDto.country = 'x1'
+        dstDto.category = srcDto.category = 'x2'
+
+        when:
+        def result = ArticleFilterMapper.map(srcDto.country, srcDto.category, srcDto.pageNumber, srcDto.pageSize, srcDto.searchPhrase)
+
+        then:
+        result == dstDto
+
+        where:
+        srcDto               | dstDto
+        [:]                  | new ArticleFilter(paginationFilter: new PaginationFilter(pageSize: 10, pageNumber: 1))
+        [searchPhrase: 'x3'] | new ArticleFilter(searchPhrase: 'x3', paginationFilter: new PaginationFilter(pageSize: 10, pageNumber: 1))
+        [pageNumber: 3]      | new ArticleFilter(paginationFilter: new PaginationFilter(pageSize: 10, pageNumber: 3))
+        [pageSize: 30]       | new ArticleFilter(paginationFilter: new PaginationFilter(pageSize: 30, pageNumber: 1))
+    }
+
+}
---
+++ web-rs/src/test/groovy/com/dwalczak/newsreader/rs/mapper/ArticleMapperSpec.groovy
@@ -0,0 +1,53 @@
+package com.dwalczak.newsreader.rs.mapper
+
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+
+class ArticleMapperSpec extends Specification {
+
+    static def SDF = new SimpleDateFormat('yyyy-MM-dd')
+
+    @Unroll
+    def "check article list mapping: #srcArticles"() {
+        given:
+        def srcDto = [totalCount: srcArticles.size(), articles: srcArticles]
+        def dstDto = [country: 'x1', category: 'x2', totalCount: dstArticles.size(), articles: dstArticles]
+
+        when:
+        def result = ArticleMapper.map(dstDto.country, dstDto.category, new com.dwalczak.newsreader.model.ArticleList(srcDto))
+
+        then:
+        result.toString() == new com.dwalczak.newsreader.rs.model.ArticleList(dstDto).toString()
+
+        where:
+        srcArticles                                                    | dstArticles
+        []                                                             | []
+        [srcArticle('x1', 'x2', 'x3', 'x4', 'x5', 'x6', '2018-03-22')] | [dstArticle('x1', 'x2', 'x3', 'x4', 'x5', 'x6', 2018, 3, 22)]
+    }
+
+    def static srcArticle(def author, def articleUrl, def title, def description, def sourceName, def imageUrl, def date) {
+        new com.dwalczak.newsreader.model.Article(
+                author: author,
+                articleUrl: articleUrl,
+                title: title,
+                description: description,
+                sourceName: sourceName,
+                imageUrl: imageUrl,
+                date: SDF.parse(date))
+    }
+    def static dstArticle(def author, def articleUrl, def title, def description, def sourceName, def imageUrl, int dateYear, int dateMonth, int dateDayOfMonth) {
+        new com.dwalczak.newsreader.rs.model.Article(
+                author: author,
+                articleUrl: articleUrl,
+                title: title,
+                description: description,
+                sourceName: sourceName,
+                imageUrl: imageUrl,
+                date: LocalDate.of(dateYear, dateMonth, dateDayOfMonth))
+    }
+
+}
